c40cd1c252d5bac40ee7868897c3706daf25a431
subhakti12 <subhaktimaulana12@gmail.com>
2024-09-05 19:13:23 +0700

    Tugas prak II

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/s', function () {
+    return view('welcome');
+});
+
+//Rute GET sederhana
+Route::get ('/hello', function (){
+    return 'Hello world!';
+});
+
+//Rute dengan parameter
+Route::get('/user/{id}', function ($id){
+    return "User ID :". $id;
+});
+
+//Rute dengan parameter opsional
+Route::get('/user/{name?}', function ($name = 'Guest'){
+    return "Hello, ". $name;
+});
+
+//Rute dengan nama
+Route::get('/profile', function (){
+    return 'This is the profile page.';
+}) -> name ('profile');
+
+//Menggunakan rute bernama untuk pengalihan
+Route::get('/redirect-to-profile', function (){
+    return redirect () -> route ('profile');
+});
