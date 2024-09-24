263be814dcae53a5986c6a11295904b779874e7e
subhakti12 <2210631170028@student.unsika.ac.id>
2024-09-24 23:03:49 +0700

    commit lagi

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view ('welcome');
+    return view('welcome');
 });
 
-//Rute GET sederhana
-Route::get ('/hello', function (){
-    return 'Hello world!';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Rute dengan parameter
-Route::get('/user/{id}', function ($id){
-    return "User ID :". $id;
-});
-
-//Rute dengan parameter opsional
-Route::get('/user/{name?}', function ($name = 'Guest'){
-    return "Hello, ". $name;
-});
-
-//Rute dengan nama
-Route::get('/profile', function (){
-    return 'This is the profile page.';
-}) -> name ('profile');
-
-//Menggunakan rute bernama untuk pengalihan
-Route::get('/redirect-to-profile', function (){
-    return redirect () -> route ('profile');
-});
+require __DIR__.'/auth.php';
